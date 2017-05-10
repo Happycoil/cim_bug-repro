62834cbbda956af9243e6f3b6ffeff842ac28602
tm <tm@aa-c0d7e-195.i04.local>
2017-05-10 17:18:14 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cim_bug_repro
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cim_bug_repro
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
